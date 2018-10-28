c8a7325c1327d255997646817b8a49edce169669
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-10-28 11:57:23 +0530

    eslint

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Header from './Header';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Header />
+        hello
       </div>
     );
   }
