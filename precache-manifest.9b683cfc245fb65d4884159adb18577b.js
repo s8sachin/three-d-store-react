7e0c47818dd572a351c584f1eb1e07fa0c07c7c3
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-11-02 20:55:09 +0530

    Updates

--- precache-manifest.9b683cfc245fb65d4884159adb18577b.js
+++ precache-manifest.9b683cfc245fb65d4884159adb18577b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/three-d-store-react/static/js/runtime~main.56c16f38.js"
   },
   {
-    "revision": "2f6a6f87a0f6bdee397e",
-    "url": "/three-d-store-react/static/js/main.2f6a6f87.chunk.js"
+    "revision": "013fbfa62b3a041b225c",
+    "url": "/three-d-store-react/static/js/main.013fbfa6.chunk.js"
   },
   {
     "revision": "a2d79ea589a7e510a8ef",
     "url": "/three-d-store-react/static/js/1.a2d79ea5.chunk.js"
   },
   {
-    "revision": "2f6a6f87a0f6bdee397e",
+    "revision": "013fbfa62b3a041b225c",
     "url": "/three-d-store-react/static/css/main.48e15144.chunk.css"
   },
   {
-    "revision": "13b0a6d5eaf41e1a6b8e5e743147d840",
+    "revision": "26fad33d12e6840ba5c92ea2a2a05168",
     "url": "/three-d-store-react/index.html"
   }
 ];
