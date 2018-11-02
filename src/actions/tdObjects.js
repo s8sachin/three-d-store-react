44b3e78d3b5261e568be56fc44efb40033b119cb
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-11-02 20:54:34 +0530

    check

--- src/actions/tdObjects.js
+++ src/actions/tdObjects.js
@@ -24,7 +24,7 @@ export const getTdObjectsAction = (skip, limit) => (
       if (response.status > 400) {
         localStorage.removeItem('token');
         localStorage.removeItem('email');
-        window.location.replace(process.env.PUBLIC_URL);
+        window.location.href = '/';
       }
     });
   }
